7f7e348ba25fd5127d80ca6196d2d3b86f9a02b5
heeds2002 <burtoneheeds2002@gmail.com>
2026-06-06 14:50:51 +0300

    Allow Netlify frontend in CORS

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,12 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5500",
       "http://127.0.0.1:5500",
-      "http://127.0.0.1:3000"
+      "https://wonderful-pudding-a84889.netlify.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
-
 app.use(express.json());
 
 // Test Route
